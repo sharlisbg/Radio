401952e655f88cf225d669e6cf3461c5a37d6155
sharlisbg <sharislizza15@gmail.com>
2023-01-13 07:47:33 -0600

    nuevos cambios

---
+++ src/ICalculator.java
@@ -0,0 +1,19 @@
+
+/*
+ * @author Sharis Barrios
+ */
+public interface ICalculator {
+    public void on(); 
+    public void off(); 
+    public void setFrequency(String freq) throws Exception;
+    public String getFrequency(); 
+    public void Forward(); 
+    public void Backward();
+    public double getFMActualStation();
+    public int getAMActualStation();
+    public void setFMActualStation (double actualStation); 
+    public void setAMActualStation (int actualStation); 
+    public void saveFMActualStation (double actualStation, int slot); 
+    public void saveAMActualStation (int actualStation, int slot); 
+
+}
