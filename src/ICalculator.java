047516f574460ee96899845fb508fac56993ba79
sharlisbg <sharislizza15@gmail.com>
2023-01-13 08:01:04 -0600

    Cambios

--- src/ICalculator.java
+++ src/ICalculator.java
@@ -4,16 +4,33 @@
  */
 public interface ICalculator {
     public void on(); 
+
     public void off(); 
+
+    public boolean isON(); 
+
     public void setFrequency(String freq) throws Exception;
+
     public String getFrequency(); 
+
     public void Forward(); 
+
     public void Backward();
+
     public double getFMActualStation();
+
     public int getAMActualStation();
+
     public void setFMActualStation (double actualStation); 
+
     public void setAMActualStation (int actualStation); 
+
     public void saveFMActualStation (double actualStation, int slot); 
-    public void saveAMActualStation (int actualStation, int slot); 
+    
+    public void saveAMActualStation (int actualStation, int slot);
+
+    public double getFMSlot(int slot); 
+
+    public int getAMSlot(int slot);
 
 }
